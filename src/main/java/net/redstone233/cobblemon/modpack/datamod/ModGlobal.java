1fc2540f1aab81718e9559fd9f2ad55ee937f361
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-05-09 17:35:31 +0800

    Update ModGlobal.java

--- src/main/java/net/redstone233/cobblemon/modpack/datamod/ModGlobal.java
+++ src/main/java/net/redstone233/cobblemon/modpack/datamod/ModGlobal.java
@@ -5,8 +5,8 @@ public class ModGlobal {
      * InnerModGlobal
      */
     public class TextModGlobal {
-        public static final String[] GLOBAL_TEXTS = {"By Redstone233\n","For Flydragons Studios\n","V1.0.0-Beat2"};
-        public static final String[] GLOBAL_BOOKS = {"Cobblemon V1.0.3\n","Minecraft V1.20.1\n", "Fabric V0.15.11"};
+        public static final String[] GLOBAL_TEXTS = {"By Redstone233","For Flydragons Studios","V1.0.0-Beat2"};
+        public static final String[] GLOBAL_BOOKS = {"Cobblemon V1.0.3","Minecraft V1.20.1", "Fabric V0.15.11"};
     }
 
     /**
@@ -14,7 +14,7 @@ public class ModGlobal {
      */
     public class TeamModGlobal {
         public static String[] Flydragons_Team = {"Redstone233"};
-        public static final String[] TRS_TEAM = {"Redstone233\n","BaixBex\n","Puyun"};
-        public static String[] Joint_Team = {"Redstone233","Tofu\n","Dreams","Xushanzhenxun"};
+        public static final String[] TRS_TEAM = {"Redstone233","BaixBex","Puyun"};
+        public static String[] Joint_Team = {"Redstone233","Tofu","Dreams","Xushanzhenxun"};
     }
 }
