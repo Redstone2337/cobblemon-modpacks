680de24f96f72ef5ba44bdeb6229eaf8f663fd0b
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-05-09 21:35:11 +0800

    Update ModGlobal.java

--- src/main/java/net/redstone233/cobblemon/modpack/datamod/ModGlobal.java
+++ src/main/java/net/redstone233/cobblemon/modpack/datamod/ModGlobal.java
@@ -5,16 +5,16 @@ public class ModGlobal {
      * InnerModGlobal
      */
     public class TextModGlobal {
-        public static final String[] GLOBAL_TEXTS = {"By Redstone233","For Flydragons Studios","V1.0.3"};
-        public static final String[] GLOBAL_BOOKS = {"Cobblemon V1.0.3","Minecraft V1.20.1", "Fabric V0.15.11"};
+        public static final String[] GLOBAL_TEXTS = {"\u00a7eBy Redstone233","\u00a7aFor Flydragons Studios","\u00a7aV1.0.0+build5"};
+        public static final String[] GLOBAL_BOOKS = {"\u00a7bCobblemonmodpack V1.0.3","\u00a7dMinecraft V1.20.1", "\u00a71Fabric V0.15.11"};
     }
 
     /**
      * InnerModGlobal
      */
     public class TeamModGlobal {
-        public static String[] Flydragons_Team = {"Redstone233"};
-        public static final String[] TRS_TEAM = {"Redstone233","BaixBex","Puyun"};
-        public static String[] Joint_Team = {"Redstone233","Tofu","Dreams","Xushanzhenxun"};
+        public static String[] Flydragons_Team = {"\u00a7eRedstone233"};
+        public static final String[] TRS_TEAM = {"\u00a7eRedstone233","\u00a7bBaixBex","P\u00a7duyun"};
+        public static String[] Joint_Team = {"\u00a7eRedstone233","\u00a7bTofu","\u00a7aDreams","\u00a7bXushanzhenxun"};
     }
 }
