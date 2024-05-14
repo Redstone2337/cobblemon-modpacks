b449c5970a9775eddb09a648eefcf73269364b63
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-05-14 20:06:32 +0800

    Update ModGlobal.java

--- src/main/java/net/redstone233/cobblemon/modpack/datamod/ModGlobal.java
+++ src/main/java/net/redstone233/cobblemon/modpack/datamod/ModGlobal.java
@@ -7,7 +7,7 @@ public class ModGlobal {
      * InnerModGlobal
      */
     public class TextModGlobal {
-        public static final String[] GLOBAL_TEXTS = {"\u00a7eBy Redstone233","\u00a7aFor Flydragons Studios","\u00a7aV1.0.0-beta4+build6"};
+        public static final String[] GLOBAL_TEXTS = {"\u00a7eBy Redstone233","\u00a7aFor Flydragons Studios","\u00a7aV1.0.0-beta4"};
         public static final String[] GLOBAL_BOOKS = {"\u00a7bCobblemonmodpack V1.0.3","\u00a7dMinecraft V1.20.1", "\u00a71Fabric V0.15.11"};
     }
 
