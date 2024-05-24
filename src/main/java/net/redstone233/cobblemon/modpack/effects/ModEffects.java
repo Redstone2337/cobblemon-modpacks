5d383d23ee0253ee0baab042894d24eef8b868e0
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-05-24 18:44:52 +0800

    Add files via upload

--- src/main/java/net/redstone233/cobblemon/modpack/effects/ModEffects.java
+++ src/main/java/net/redstone233/cobblemon/modpack/effects/ModEffects.java
@@ -10,13 +10,15 @@ import net.redstone233.cobblemon.modpack.ModpackTestMod;
 public class ModEffects {
 
     public static final StatusEffect FIRE_STATUS_EFFECT = registerModEffect("fire_on_body", new FireEffect(StatusEffectCategory.HARMFUL,0xFF8247));
-    public static final StatusEffect GOOD_FIRE_STATUS_EFFECT = registerModEffect("good_fire_on_body", new FireEffect(StatusEffectCategory.HARMFUL,0xFF8247));
-    public static final StatusEffect GREAT_FIRE_STATUS_EFFECT = registerModEffect("great_fire_on_body", new FireEffect(StatusEffectCategory.HARMFUL,0xFF8247));
-    public static final StatusEffect EXP_STATUS_EFFECT = registerModEffect("experience", new FireEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
-    public static final StatusEffect GOOD_EXP_STATUS_EFFECT = registerModEffect("good_experience", new FireEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
-    public static final StatusEffect GREAT_EXP_STATUS_EFFECT = registerModEffect("great_experience", new FireEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
-    public static final StatusEffect BETTER_EXP_STATUS_EFFECT = registerModEffect("better_experience", new FireEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
-    public static final StatusEffect BEST_EXP_STATUS_EFFECT = registerModEffect("best_experience", new FireEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
+    public static final StatusEffect GOOD_FIRE_STATUS_EFFECT = registerModEffect("good_fire_on_body", new GoodFireEffect(StatusEffectCategory.HARMFUL,0xFF8247));
+    public static final StatusEffect GREAT_FIRE_STATUS_EFFECT = registerModEffect("great_fire_on_body", new GreatFireEffect(StatusEffectCategory.HARMFUL,0xFF8247));
+    
+    public static final StatusEffect EXP_STATUS_EFFECT = registerModEffect("experience", new ExpEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
+    public static final StatusEffect GOOD_EXP_STATUS_EFFECT = registerModEffect("good_experience", new GoodExpEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
+    public static final StatusEffect GREAT_EXP_STATUS_EFFECT = registerModEffect("great_experience", new GreatExpEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
+    public static final StatusEffect BETTER_EXP_STATUS_EFFECT = registerModEffect("better_experience", new BetterExpEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
+    public static final StatusEffect BEST_EXP_STATUS_EFFECT = registerModEffect("best_experience", new BestExpEffect(StatusEffectCategory.BENEFICIAL,0x98D982));
+
 
 
     private static final StatusEffect registerModEffect(String name,StatusEffect effect) {
